bfcea258ab74f6bd9893e3de0331236e4258f641
Joseperti <joseperti@users.noreply.github.com>
2019-06-06 16:22:55 -0500

    Lower case for Context query

--- macroModelsTool/views.py
+++ macroModelsTool/views.py
@@ -74,11 +74,11 @@ class Persistencia:
         self.loadVarsOptions()
         self.availablePortfolios()
         self.loadScenarios()
-        #self.loadContexts()
+        self.loadContexts()
 
     def loadContexts(self):
         print("Loading Contexts")
-        output1 = pd.read_sql("select context_name from %scontextlist order by context_name" %(schema),con)
+        output1 = pd.read_sql("select context_name from %scontextlist order by context_name" %(schema),self.con)
         self.contextList = list(output1["context_name"].values)
         print(self.contextList)
 
